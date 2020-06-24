3ee702a60c1af6b795305a05474645873b3d67ad
CE <archmagece@gmail.com>
2020-06-24 17:54:59 +0900

    new gem project

---
+++ lib/storm_trooper.rb
@@ -0,0 +1,6 @@
+require "storm_trooper/version"
+
+module StormTrooper
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/storm_trooper/version.rb
@@ -0,0 +1,3 @@
+module StormTrooper
+  VERSION = "0.1.0"
+end
