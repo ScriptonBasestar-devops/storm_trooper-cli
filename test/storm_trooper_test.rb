52e4c061959ad0ded4262091b805b1b8fcfbb56c
CE <archmagece@gmail.com>
2020-06-29 14:49:00 +0900

    render template check

--- test/storm_trooper_test.rb
+++ test/storm_trooper_test.rb
@@ -1,13 +1,29 @@
 # frozen_string_literal: true
 
 require 'test_helper'
+require 'erb'
 
 class StormTrooperTest < Minitest::Test
   def test_that_it_has_a_version_number
     refute_nil ::StormTrooper::VERSION
   end
 
-  def test_it_does_something_useful
-    assert false
+  # include Thor::Base
+  # What we're interested in…
+  # include Thor::Actions
+  def test_rendering
+    @config = {
+      name: 'name11',
+      underscored_name: 'iiii'
+    }
+    puts File.absolute_path('.')
+    @vars = YAML.load(File.read('test/vars/docker-compose/db.yml'))
+    # config.merge!(cloud_config)
+
+    result = YAML.safe_load(ERB.new(File.read("test/tmpls/docker-compose/db.yml")).result(binding))
+    puts result
+    puts YAML.dump(result)
+    # thor.template("tmpls/docker-compose/db.yml.tt", "tmp1.yml", vars)
   end
+
 end
