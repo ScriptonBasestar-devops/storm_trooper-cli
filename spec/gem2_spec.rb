96c1283efa2946f256ac38663b142c05f0ae2972
CE <archmagece@gmail.com>
2020-06-29 15:12:06 +0900

    rspec test

--- spec/gem2_spec.rb
+++
@@ -1,9 +0,0 @@
-RSpec.describe Gem2 do
-  it "has a version number" do
-    expect(Gem2::VERSION).not_to be nil
-  end
-
-  it "does something useful" do
-    expect(false).to eq(true)
-  end
-end
---
+++ spec/storm_trooper_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe StormTrooper do
+  it 'has a version number' do
+    expect(StormTrooper::VERSION).not_to be nil
+  end
+
+  it 'does something useful' do
+    expect(false).to eq(true)
+  end
+end
