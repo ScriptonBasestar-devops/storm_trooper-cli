48e6ec11ebf0da7ef7329779784f0ce1d73ac38c
CE <archmagece@gmail.com>
2020-07-05 17:17:38 +0900

    fragment only for use git

--- lib/storm_trooper/fragment/git.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-module StormTrooper
-  module Fragment
-    class Git < Base
-
-    end
-  end
-end
--- lib/storm_trooper/fragment/root.rb
+++ lib/storm_trooper/fragment/root.rb
@@ -1,13 +1,37 @@
 # frozen_string_literal: true
 
-require_relative 'git'
-
 module StormTrooper
   module Fragment
     class Root < Base
 
-      desc 'clone', 'Generate IaC from configuration'
-      subcommand 'clone', StormTrooper::Fragment::Git
+      method_option :env, type: :string, desc: 'Environment', required: true, default: ENV['STROOP_ENV']
+      method_option :version, type: :string, desc: 'Version', aliases: '-v', required: false
+      desc 'fragment https://github.com/foo/bar-module module1', 'Add Fragment for StormTrooper'
+      def add(uri, path)
+        say 'Fragment add - git submodule'
+        run! "git submodule add #{uri} #{path}"
+        run! 'git submodule init'
+        run! 'git submodule update'
+
+      end
+
+      desc 'git submodule add https://github.com/foo/bar-module', 'Add Fragment for StormTrooper'
+      def update(path)
+        say 'Fragment update - reset and update'
+      end
+
+      desc 'git submodule add https://github.com/foo/bar-module', 'Add Fragment for StormTrooper'
+      def modify
+        say 'Fragment modify - This is not yet supported. Do it manually.', :yellow
+      end
+
+      desc 'git submodule deinit ', 'Add Fragment for StormTrooper'
+      def remove(path)
+        say 'Fragment remove - remove from project'
+        run! "git submodule deinit -f -- a/#{path}"
+        run! "rm -rf .git/modules/a/#{path}"
+        run! "git rm -f a/#{path}"
+      end
 
     end
   end
